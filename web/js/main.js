035741db22f072e504a7800c4f75226f9214391a
rob <rob@codebox.org.uk>
2020-06-15 13:25:41 +0100

    elevation colouring

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,9 @@
 function init(seed){
     "use strict";
 
-    const RENDER_SCALE = 10,
-        MODEL_WIDTH = 100,
-        MODEL_HEIGHT = 100,
+    const RENDER_SCALE = 1,
+        MODEL_WIDTH = 1000,
+        MODEL_HEIGHT = 1000,
         ZOOM_LEVEL = 7,
         rnd = randomFromSeed(seed),
         model = buildModel(rnd, MODEL_WIDTH, MODEL_HEIGHT, ZOOM_LEVEL),
--- web/js/view.js
+++ web/js/view.js
@@ -3,10 +3,27 @@ function buildView(scale) {
     const elCanvas = document.getElementById('canvas');
     let canvas;
 
-    function getElevationColour(elevation) {
-        console.assert(elevation >= -1 && elevation <= 1);
-        return `rgba(0,0,0,${0.5 + elevation/2})`;
+    function buildRangeShifter(minIn, maxIn, minOut, maxOut) {
+        return v => {
+            console.assert(v>= minIn && v<= maxIn, v);
+            return minOut + (maxOut - minOut) * (v - minIn) / (maxIn - minIn);
+        };
     }
+
+    const getElevationColour = (() => {
+        const SEA_LEVEL = 0,
+            getSeaLightness = buildRangeShifter(-1, 0, 0, 50),
+            getGroundLightness = buildRangeShifter(0, 1, 20, 80);
+
+        return (elevation) => {
+            console.assert(elevation >= -1 && elevation <= 1);
+            if (elevation < SEA_LEVEL) {
+                return `hsl(220,100%,${getSeaLightness(elevation)}%)`;
+            } else {
+                return `hsl(115,100%,${getGroundLightness(elevation)}%)`;
+            }
+        };
+    })();
 
     function drawElevationSquare(x, y, v) {
         canvas.drawRectangle(x * scale, y * scale, scale, scale, getElevationColour(v));
